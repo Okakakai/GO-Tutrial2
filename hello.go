d4538e73aec505d137e6baad15d9bf2b82ab3789
Okakakai <inoue.kai.work@gmail.com>
2022-03-09 13:01:39 +0900

    hello

---
+++ hello.go
@@ -0,0 +1,21 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello")
+// }
+
+// add TDD
+
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "HELLO"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
